a6af5adbac729909ce27abcfa3222ec3418232b5
unknown <alexandreroffiaen@gmail.com>
2022-07-21 08:51:15 +0200

    transformation en cv

--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,16 @@
 const Home = () => {
     return (
         <div className="Home">
-            <h1>Home</h1>
+            <h1>Alexandre ROFFIAEN</h1>
+            <div className="cv-flex">
+                <div>
+                    <h2>FORMATION</h2>
+                    <ul>
+                        <li>ma formation</li>
+                        <li>ma formation</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
